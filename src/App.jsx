e4b7534de500f0b856be0315f306b735a3695c20
dendun-nf <deni.rianto0308@gmail.com>
2023-12-07 08:47:50 +0700

    include header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-// import Header from "./components/Header.jsx";
+import Header from "./components/Header.jsx";
 import Content from "./components/Content.jsx";
 import Footer from "./components/Footer.jsx";
 
 function App() {
 	return (
 		<>
-			{/* <Header /> */}
+			<Header />
 			<Content />
 			<Footer />
 		</>
