3a0a7e48518588261c84c78ed9e35468b0803176
dendun-nf <deni.rianto0308@gmail.com>
2023-12-11 05:08:49 +0700

    change to default dist instead of docs

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import macrosPlugin from "vite-plugin-babel-macros";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	build: {
-		// for github pages
-		outDir: "docs",
-	},
 	plugins: [react(), macrosPlugin()],
 });
