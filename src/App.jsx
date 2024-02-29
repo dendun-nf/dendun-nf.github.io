19f52d88a546e74931ffed9b2178b3148c2267b0
dendun-nf <deni.rianto0308@gmail.com>
2024-02-29 14:40:53 +0700

    Purge and use typescript

--- src/App.jsx
+++
@@ -1,5 +0,0 @@
-import Navbar from "./components/navbar/Navbar";
-
-function App() {}
-
-export default App;
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<>
+			<h1 className="text-4xl font-bold text-center">Under Maintenance</h1>
+		</>
+	);
+}
+
+export default App;
